fb0857823a15637f9a0f506089fceba9f9c3e2f9
pj <3320552956@qq.com>
2017-07-15 11:07:15 +0800

    资源

---
+++ MahjonClient/Assets/Scripts/BattleUI/PaiListSort.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PaiCompare : IComparer<Pai>
+{
+    //ComparetTo:大于 1； 等于 0； 小于 -1；  
+    public int Compare(Pai data1, Pai data2)
+    {
+        return data1.Id.CompareTo(data2.Id);
+    }
+}
